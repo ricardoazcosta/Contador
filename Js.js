1afab67a553ec37956ec912c31250dce68d8eedc
ricardoazcosta <ricardozanete@gmail.com>
2022-07-25 16:18:05 -0300

    primeiro contador

---
+++ Js.js
@@ -0,0 +1,31 @@
+/*
+var currentNumberWrapper = document.getElementById("currentNumber")
+var currentNumber=0;
+
+function increment() {
+    currentNumber=currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+*/
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
